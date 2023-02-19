545506af953f4f71c7d9a3b5420402f36d31427f
cw-barry <barry@clarusway.com>
2023-02-18 19:12:21 -0500

    update static

--- main/urls.py
+++ main/urls.py
@@ -25,8 +25,7 @@ urlpatterns = [
     path('account/', include('accounts_app.urls'))
 ]
 
-if settings.DEBUG:
-    urlpatterns += \
+urlpatterns += \
     static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
     static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
